c0013006f5dba647f9be4f5187499db0c46adb42
AstroLad <sitecoder@icloud.com>
2013-12-23 23:54:58 +1100

    Added Code

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+
+         # ============================ # # ================================================================ #
+         # # Aspergers Edition Z v0.5 # # # THIS SOURCE IS FULLY CUSTOM.                                     #
+         # ============================ # # THIS SOURCE MAY APPEAR TO BE USING OTHER SOURCES CODE TRUE.      #
+         # Created By AstroLad          # # THIS SOURCE CAN BE EDITED BY YOU 100% TRUE.                      #
+         # Plugins By AstroLad          # # THIS SOURCE CAN BE CLONED & REUSED FOR ANY LEARNING PURPOSES.    #
+         # Open Source Applies Here     # # THIS SOURCE IS NOT RESPONSIBLE FOR THE OUTPUT A CLIENT PRODUCES. #
+         # ============================ # # ================================================================ #
+         
+    PD9waHANCiANCiAgIyBCdWlsZGluZyBhcm91bmQgdGhpcyBpcyBwb3NzaWJsZS4NCiAgIyBFYWNoIGZpbGUgaGFzIHRoZWlyIG93biBjdXN0b20g
+              Y3NzLg0KICAjIH5Bc3Ryb0xhZH4NCiAgDQogICBpbmNsdWRlX29uY2UoJ25hdm1lbnUucGhwJyk7CQkgDQo/Pg==
